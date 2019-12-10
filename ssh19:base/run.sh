51cae8634e9b31d0f0681e69b64793056253548c
Miguel Amorós <miguel14amoros@gmail.com>
2019-12-10 10:43:46 +0100

    ssh19:base

--- ssh19:base/run.sh
+++ ssh19:base/run.sh
@@ -2,3 +2,6 @@
 # ASIX M06 - MIGUEL AMOROS
 
 docker run --rm --name ldapserver -h ldapserver --net ldapnet -d isx46410800/ldapserver19:group
+
+docker run --rm --name ssh -h ssh --net ldapnet -p 2022:22 -v homes:/tmp/home --privileged -d isx46410800/ssh19:base
+
